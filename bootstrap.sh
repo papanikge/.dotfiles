d6bcc83b7ade61cdd24af1f695546dea46bd98c3
George Papanikolaou <gpapanikolaou@skroutz.gr>
2019-03-29 12:26:16 +0200

    Revamp bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,38 +1,30 @@
-# let's remove the previous versions first
-rm ../.bashrc
-rm ../.bash_profile
+#!/bin/bash
+#
+# Only ultra-essential files are symlinked
+#
 
-# [Obsoletes] password-sensitive files
-# scp papanikge@diogenis.ceid.upatras.gr:.irc.conf .weechat/irc.conf
-# scp papanikge@diogenis.ceid.upatras.gr:.offlineimaprc .offlineimaprc
-# scp papanikge@diogenis.ceid.upatras.gr:.msmtp-ceid.gpg .msmtp-ceid.gpg
+set -eu
 
-cd
+cd $HOME
+ln -s .dotfiles/.zshrc .
 ln -s .dotfiles/.vim .
 ln -s .dotfiles/.vimrc .
 ln -s .dotfiles/.mutt .
 ln -s .dotfiles/.muttrc .
-ln -s .dotfiles/.bashrc .
-ln -s .dotfiles/.bash_prompt .
 ln -s .dotfiles/.skroutz-helpers .
 ln -s .dotfiles/.aliases .
-ln -s .dotfiles/.bash_profile .
-ln -s .dotfiles/.zshrc .
 ln -s .dotfiles/.gitconfig .
-ln -s .dotfiles/.ghci .
-ln -s .dotfiles/.agignore .
-ln -s .dotfiles/.valgrindrc .
-ln -s .dotfiles/.wgetrc .
 ln -s .dotfiles/.weechat .
 ln -s .dotfiles/.irbrc .
-if [[ `uname -s` == 'Linux' ]]; then
-  ln -s .dotfiles/.inputrc .
-  ln -s .dotfiles/.Xdefaults .
-  ln -s .dotfiles/.Xmodmap .
-  ln -s .dotfiles/.dunstrc .
-  ln -s .dotfiles/.gtkrc-2.0 .
-  ln -s .dotfiles/.msmtprc .
-  ln -s .dotfiles/.offlineimaprc .
-  ln -s .dotfiles/.xinitrc .
-  ln -s .dotfiles/.xmonad .
-fi
+
+# Secrets and sensitive files
+# TODO need to fetch password-sensitive files
+
+# oh-my-tmux
+wget -qO .tmux.conf https://raw.githubusercontent.com/gpakosz/.tmux/master/.tmux.conf
+
+
+echo "You might need to run:"
+echo "    adduser <username>"
+echo "...then add to sudoers, switch to them and run:"
+echo "    sudo chsh -s /bin/zsh"
